d9618f4d97eb441e16eadd641eb035da172ef3eb
WaitMoonMan <1033404553@qq.com>
2017-09-25 08:47:22 +0800

    '添加sphinx扩展'

---
+++ Kernel/App/Extensions/Sphinx.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kernel\App\Extensions;
+
+
+use Kernel\App\Common\Extendtion;
+
+class Sphinx extends Extendtion
+{
+    protected $mapUrl = array(
+        '5.6-nts-vc11-x86' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.6-nts-vc11-x86.zip',
+        '5.6-ts-vc11-x86' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.6-ts-vc11-x86.zip',
+        '5.6-nts-vc11-x64' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.6-nts-vc11-x64.zip',
+        '5.6-ts-vc11-x64' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.6-ts-vc11-x64.zip',
+
+        '5.5-nts-vc11-x86' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.5-nts-vc11-x86.zip',
+        '5.5-ts-vc11-x86' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.5-ts-vc11-x86.zip',
+        '5.5-nts-vc11-x64' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.5-nts-vc11-x64.zip',
+        '5.5-ts-vc11-x64' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.5-ts-vc11-x64.zip',
+
+        '5.4-nts-vc9-x86' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.4-nts-vc9-x86.zip',
+        '5.4-ts-vc9-x86' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.4-ts-vc9-x86.zip',
+
+        '5.3-nts-vc9-x86' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.3-nts-vc9-x86.zip',
+        '5.3-ts-vc9-x86' => 'http://windows.php.net/downloads/pecl/releases/sphinx/1.3.3/php_sphinx-1.3.3-5.3-ts-vc9-x86.zip'
+    );
+
+    protected $dllName = 'php_sphinx.dll';
+
+
+
+}
